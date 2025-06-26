92c39c7ba6208c8388e415fddc7d65d5945f4233
jdjtan <jdjtan01@gmail.com>
2025-06-26 20:36:16 +0800

    Finishing touches to frontend

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -26,6 +26,26 @@ const Orders = ({ url }) => {
     }
   };
 
+  const statusHandler = async (event, orderId) => {
+    const response = await axios.post(url+"/api/order/status", {
+      orderId,
+      status: event.target.value
+    });
+
+    if (response.data.success) {
+      await fetchAllOrders();
+
+      toast.success("Order status updated", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: false,
+      });
+    }
+  }
+
   useEffect(() => {
     fetchAllOrders();
   }, []);
@@ -57,7 +77,7 @@ const Orders = ({ url }) => {
             </div>
             <p>Items : {order.items.length}</p>
             <p>${order.amount}</p>
-            <select>
+            <select onChange={(event) => statusHandler(event, order._id)} value={order.status}>
               <option value="Processing Order">Processing Order</option>
               <option value="Out for delivery">Out for delivery</option>
               <option value="Delivered">Delivered</option>
