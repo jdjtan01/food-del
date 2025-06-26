2efe2088a4d9b445865af9fb75d1ed06a5a56f08
jdjtan <jdjtan01@gmail.com>
2025-06-26 12:44:06 +0800

    Add user cart functionality to front end

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -10,16 +10,24 @@ const StoreContextProvider = (props) => {
   const [token, setToken] = useState('');
   const [food_list, setFoodList] = useState([]);
 
-  const addToCart = (itemId) => {
+  const addToCart = async (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
     } else {
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
+
+    if(token) {
+      await axios.post(url+"/api/cart/add", {itemId}, {headers:{token}})
+    }
   };
 
-  const removeFromCart = (itemId) => {
+  const removeFromCart = async (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+
+    if(token) {
+      await axios.post(url+"/api/cart/remove", {itemId}, {headers:{token}})
+    }
   };
 
   const getTotalCartAmount = () => {
@@ -38,12 +46,19 @@ const StoreContextProvider = (props) => {
     setFoodList(response.data.data);
   }
 
-  // to persist token even after refresh
+  // fetch users cart data
+  const loadCartData = async(token) => {
+    const response = await axios.post(url+"/api/cart/get", {}, {headers:{token}});
+    setCartItems(response.data.cartData);
+  }
+
+  // to persist token even after refresh. also to persist user cart data
   useEffect(() => {
     async function loadData() {
       await fetchFoodList();
       if(localStorage.getItem("token")) {
       setToken(localStorage.getItem("token"));
+      await loadCartData(localStorage.getItem("token"));
       }
     }
     loadData();
